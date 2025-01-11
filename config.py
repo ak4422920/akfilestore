7465fdb73bfe4c3492a4dab5a0cf87d66256b50e
ak4422920 <ak4422920@gmail.com>
2025-01-11 06:28:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -64,10 +64,10 @@ SHORTLINK_API = environ.get("SHORTLINK_API", "4ed9f7b048d0c13394d7799f605e51a11d
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
 
 # Website Info:
-WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', False)) # Set True or False
+WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', True)) # Set True or False
 
 # If Website Url Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-WEBSITE_URL = environ.get("WEBSITE_URL", "https://akmovieshubbackup.blogspot.com/2025/01/akmovieshubbackup.html") # For More Information Check Video On Yt - @Tech_VJ
+WEBSITE_URL = environ.get("WEBSITE_URL", "https://akmovieshubbackup.blogspot.com/2025/01/akmovieshubbackup_10.html") # For More Information Check Video On Yt - @Tech_VJ
 
 # File Stream Config
 STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or False
